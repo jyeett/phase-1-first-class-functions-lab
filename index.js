03afee4a9de5338dcf4bdd1157fc6f4b645d4bfc
Jordan Yee <jordanyee799@gmail.com>
2021-10-13 10:58:47 -0700

    commit tree b487e2c8fb5d42e8af02bc27c0865c83e222c090

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return (fare) => fare * multiplier;
+}
+
+// const fareDoubler = createFareMultiplier(2);
+
+function fareDoubler(fare) {
+    const x = createFareMultiplier(2)
+    return x(fare);
+}
+
+function fareTripler(fare) {
+    const x = createFareMultiplier(3)
+    return x(fare);
+}
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers);
+}
